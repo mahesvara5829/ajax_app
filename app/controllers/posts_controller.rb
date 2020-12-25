91920a059bcf9b9ccce2c1ddc9aa7df7d1a54418
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2020-12-25 16:24:54 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,8 @@ end
 
 
 def create
-  Post.create(content: params[:content])
-  redirect_to action: :index
+  post = Post.create(content: params[:content], checked: false)
+  render json:{ post: post }
 end
 
 def checked
